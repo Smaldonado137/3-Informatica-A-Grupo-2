18a0c68bcbf21e782a759aae10b9a7302bed00cf
Smaldonado137 <santiago.maldonado@cbecheverria.edu.ec>
2024-11-04 10:50:35 -0500

    .

--- Game/src/index.js
+++ Game/src/index.js
@@ -1,13 +1,14 @@
 import { Preloads } from './preloads.js';
 import { Game } from './game.js';
 import { Pause } from './pause.js';
+import { Winner } from './winner.js';
 
 const config = {
     type: Phaser.AUTO,
     width: 1450,
     height: 850,
     parent: 'Preloads',
-    scene: [Preloads, Game, Pause],
+    scene: [Preloads, Game, Pause, Winner],
     physics: {
         default: 'arcade',
         arcade: {    
---
+++ Game/src/winner.js
@@ -0,0 +1,14 @@
+export class Winner extends Phaser.Scene {    
+    
+    constructor(){
+        super({ key: 'Winner' });
+        
+    }
+    
+    create(){
+        const widthScr = this.scene.settings.data.widthScreen;
+        const heightScr = this.scene.settings.data.heightScreen;
+        
+    }
+
+}
